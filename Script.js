eac0a7963b4999e0b90fa2202342968d85a52770
Synth-Axis <36264305+Synth-Axis@users.noreply.github.com>
2023-04-10 14:33:00 +0100

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,38 +1,30 @@
 window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-  }
+      window.scrollTo(0, 0);
+      }
+
+//Nav Links
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach(element =>{
+      if (element.href.includes(activePage)){
+            element.classList.add("nav-link-active");
+      }
+})
 
 
- /* Choose Form */
+//Contact Forms
+let selectedTab = document.querySelectorAll(".form-link"); //Selecionar as opções
+selectedTab.forEach(element => element.addEventListener("click", eventArg => { //Por cada uma,
+      let contents = document.querySelectorAll(".form-content"); //selecionar o conteudo associado
+      contents.forEach( content => content.classList.add("hidden")); //Por cada conteudo -> esconder
+      let tabClicked = eventArg.target; // Opção cliclada é igual ao target do array das opções
+      let tabId = tabClicked.id; // Criar variavel auxiliar com o ID da opção clicada
+      let showContent = document.querySelector("." + tabId); // Associar o ID da opção clicada à classe do conteudo com o mesmo nome
+      showContent.classList.remove("hidden"); // Remover o hide do conteudo selecionado
+      eventArg.preventDefault(); //Impedir o comportamento do link
+}));
 
-  const formJoin = document.getElementById("formJoinUs");
-  const linkJoin = document.getElementById("showJoinUs");
 
-  linkJoin.addEventListener("click", function(event) {
-        formPartner.style.display = "none";
-        formHi.style.display = "none";
-        event.preventDefault();
-        formJoin.style.display = "block";
-  });
-
-  const formPartner = document.getElementById("formPartnership");
-  const linkPartner = document.getElementById("showPartnership");
-
-  linkPartner.addEventListener("click", function(event) {
-        formJoin.style.display = "none";
-        formHi.style.display = "none";
-        event.preventDefault();
-        formPartner.style.display = "block";
-  });
-
-  const formHi = document.getElementById("formSayHi");
-  const linkHi = document.getElementById("showSayHi");
-
-  linkHi.addEventListener("click", function(event) {
-        formJoin.style.display = "none";
-        formPartner.style.display = "none";
-        event.preventDefault();
-        formHi.style.display = "block";
-  });
 
 
